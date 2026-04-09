fef664664ddfcaf5c67c01ad6801296250358d45
Manon Siohan <manon.siohan@gmail.com>
2026-04-09 18:31:56 +0200

    feat: int_customers_enriched rfm_score, niveau and some tests

---
+++ models/intermediate/int_customers_enriched.sql
@@ -0,0 +1,80 @@
+WITH main_address_ranked AS (
+	SELECT
+		customer_id
+		, rue 
+		, ville 
+		, code_postal
+		, pays
+        , row_number() over(
+            partition by customer_id 
+            order by address_id) AS rn
+	FROM {{ref('stg_addresses')}} 
+	WHERE type_address = 'principale'
+), 
+
+main_address AS (
+    SELECT * FROM main_address_ranked
+    WHERE rn = 1
+),
+
+customer_segments_ranked AS (
+	SELECT
+		customer_id
+		, rfm_score
+		, segment_label 
+		, total_achats_ht 
+		, nb_commandes
+		, row_number() over(
+			partition by customer_id
+			order by date_calcul DESC) AS rn
+	FROM {{ref("stg_customers_segments")}}
+),
+
+customer_segments AS (
+	SELECT *
+	FROM customer_segments_ranked
+	WHERE rn = 1
+),
+
+loyalty_accounts_ranked AS (
+	SELECT
+		customer_id
+		, points_solde
+		, niveau 
+		, row_number() over(
+			partition by customer_id 
+			order by date_adhesion DESC) AS rn
+ 	FROM {{ ref("stg_loyalty_accounts")}}
+ ), 
+ 
+ loyalty_accounts AS (
+ 	SELECT * 
+ 	FROM loyalty_accounts_ranked
+ 	WHERE rn = 1
+ )
+ 
+SELECT
+	c.customer_id
+	, c.nom
+	, c.prenom 
+	, c.email 
+	, c.telephone
+	, ma.rue
+	, ma.ville
+	, ma.pays
+	, c.date_inscription 
+	, c.date_naissance 
+	, try_cast(c.newsletter AS BOOL) AS newsletter
+	, c.segment
+	, cs.rfm_score
+	, cs.segment_label 
+	, cs.total_achats_ht
+	, cs.nb_commandes
+	, la.points_solde
+	, la.niveau
+	
+
+FROM high_peak.dev_staging.stg_customers c 
+LEFT JOIN main_address ma ON c.customer_id = ma.customer_id
+LEFT JOIN customer_segments cs ON c.customer_id = cs.customer_id
+LEFT JOIN loyalty_accounts la ON c.customer_id = la.customer_id
